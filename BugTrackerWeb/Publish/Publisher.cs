c4451682441542d40956e665d5e20d8d02a5137e
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2019-05-09 19:38:25 -0300

    Adicionando o publisher

---
+++ BugTrackerWeb/Publish/Publisher.cs
@@ -0,0 +1,37 @@
+﻿using BugTrackerWeb.Models;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTrackerWeb.Publish
+{
+    public class Publisher
+    {
+        const string ServiceBusConnectionString = "";
+        const string QueueName = "bugtracker-queue";
+        private IQueueClient queueClient;
+
+        public Publisher()
+        {
+            this.queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+        }
+
+        public async Task SendMessage(BugViewModel model)
+        {
+            try
+            {
+                var body = JsonConvert.SerializeObject(model);
+                var message = new Message(Encoding.UTF8.GetBytes(body));
+                await this.queueClient.SendAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
